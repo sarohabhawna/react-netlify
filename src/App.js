dc2da774e3a100cf672a26ef3314e2f0fd1d74fc
Bhawna <bhawna.saroha@catalyst.sh>
2019-03-29 17:57:35 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import BadPage from './404';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={ Home } />
+                    <Route component={ BadPage } />
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={ logo } className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+          </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+          </a>
+                    <Link className="App-link" to="/invalid">Invalid Route</Link>
+                    <p>
+                        <a className="App-link" href="/admin/">NetlifyCMS Admin</a>
+                    </p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Home;
