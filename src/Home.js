159534e9bf7aa7a96b9b663ae02680313dec6e8a
Bhawna <bhawna.saroha@catalyst.sh>
2019-04-02 12:23:20 +0530

    function created to search in md file

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,53 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
 
 class Home extends Component {
+    constructor () {
+        super();
+        this.state = {
+            markdown: '',
+            title: ''
+        }
+    }
+    componentDidMount() {
+        const mdPath = require("./test.md");
+
+        fetch(mdPath)
+            .then(response => {
+                return response.text()
+            })
+            .then(text => {
+                this.setState({
+                    markdown: marked(text)
+                })
+            });
+
+    }
+
+    componentDidUpdate(previousState) {
+        if (this.state !== previousState) {
+            let { markdown: mdData } = this.state;
+            let stringTitleSearchPattern = /title:/i;
+            let indexOfTitle = mdData.search(stringTitleSearchPattern);
+            let titleToEndFile = mdData.substr(indexOfTitle);
+            let lineBreak = /\n/;
+            let lineBreakIndex = titleToEndFile.search(lineBreak);
+            let startIndex = 7;
+            let endIndex = lineBreakIndex - 5;
+            let title = titleToEndFile.substring(startIndex, endIndex);
+
+            if (title !== this.state.title) {
+                this.setState({ title })
+            } else {
+                return;
+            }
+        }
+    }
+
     render() {
+        const { markdown, title } = this.state;
         return (
             <div className="App">
                 <header className="App-header">
@@ -24,6 +68,12 @@ class Home extends Component {
                     <p>
                         <a className="App-link" href="/admin/">NetlifyCMS Admin</a>
                     </p>
+                    <section>
+                        <article>
+                            <h1>{ title }</h1>
+                            <div dangerouslySetInnerHTML={{__html: markdown}}></div>
+                        </article>
+                    </section>
                 </header>
             </div>
         );
---
+++ src/test.md
@@ -0,0 +1,8 @@
+---
+path: loremipsum
+date: 2019-03-29T10:33:28.005Z
+title: Lorem Ipsum Dolor Set
+---
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+<img src="https://miro.medium.com/max/1120/1*dLaDL-lSN0iprzmOpmM7zQ.png"/>
